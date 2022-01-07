13388abf2ef3721cb3bb6d7f58b4174e865fa6ab
Moses Wankis <mwankis@gmail.com>
2022-01-07 08:29:07 +0200

    khusi project

---
+++ Khusi.Sales.Implementation/Crm.Sales.Webresources/src/scripts/contactform.js
@@ -0,0 +1,14 @@
+﻿function onLoad(executionContext) {
+    var formContext = executionContext.getFormContext();
+    
+}
+
+function onContactTypeChange(executionContext) {
+    var formContext = executionContext.getFormContext();
+    var projectStartDate = formContext.getAttribute("wh_startdate").getValue();
+    if (projectStartDate != null) {
+        let projectYear = projectStartDate.getFullYear();
+        formContext.getAttribute("wh_projectyear").setValue(projectYear);
+        formContext.getControl("wh_projectyear").setDisabled(true);
+    }
+}
